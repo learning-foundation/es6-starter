612c824c9d43c44bf1821598d596ec3fb584ee54
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-12-01 11:59:07 -0200

    rendering list of repositories

--- src/app/main.js
+++ src/app/main.js
@@ -2,6 +2,7 @@ class App {
   constructor() {
     this.repositories = [];
     this.formEl = document.getElementById("repo-form");
+    this.listEl = document.getElementById("repo-list");
     this.registerHandlers();
   }
 
@@ -18,7 +19,35 @@ class App {
       html_url: "https://github.com/vinicius-serpa"
     });
 
-    console.log(this.repositories);
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = "";
+
+    this.repositories.forEach(repo => {
+      let imageEl = document.createElement("img");
+      imageEl.setAttribute("src", repo.avatar_url);
+
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.setAttribute("href", repo.html_url);
+      linkEl.appendChild(document.createTextNode("Access"));
+
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imageEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
