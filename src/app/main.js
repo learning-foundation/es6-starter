572bedc8627e4a7daf838e7abc0bd4f4809ff4a5
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-12-02 17:12:28 -0200

    retrieving repository data with axios

--- src/app/main.js
+++ src/app/main.js
@@ -1,7 +1,10 @@
+import api from "./api";
+
 class App {
   constructor() {
     this.repositories = [];
     this.formEl = document.getElementById("repo-form");
+    this.inputEl = document.querySelector("input[name=repository]");
     this.listEl = document.getElementById("repo-list");
     this.registerHandlers();
   }
@@ -10,13 +13,27 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault();
+
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0) return;
+
+    const response = await api.get(`/repos/${repoInput}`);
+
+    const {
+      name,
+      description,
+      html_url,
+      owner: { avatar_url }
+    } = response.data;
+
     this.repositories.push({
-      name: "vinicius-serpa",
-      description: "description",
-      avatar_url: "https://avatars1.githubusercontent.com/u/6933158?s=96&v=4",
-      html_url: "https://github.com/vinicius-serpa"
+      name,
+      description,
+      avatar_url,
+      html_url
     });
 
     this.render();
